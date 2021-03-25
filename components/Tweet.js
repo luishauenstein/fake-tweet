0cc6de07b4556d6c84327cc6a2fa5d4670480886
Luis Hauenstein <account@luishauenstein.com>
2021-03-25 15:31:23 +0100

    set foreign object rendering to true

--- components/Tweet.js
+++ components/Tweet.js
@@ -446,6 +446,7 @@ const Tweet = (props) => {
       //becuase profile image comes from another origin
       allowTaint: true,
       useCORS: true,
+      foreignObjectRendering: true,
     };
 
     html2canvas(node, param).then((canvas) => {
